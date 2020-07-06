2a838b80ba8ccfecbf2c2bdf8bae736c199c4542
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2020-07-06 16:08:30 -0300

    Initial declarations.

--- Delphi.Rest.Web.Service.Component.pas
+++ Delphi.Rest.Web.Service.Component.pas
@@ -2,13 +2,54 @@
 
 interface
 
-uses System.Classes;
+uses System.Classes, Web.HTTPApp;
 
 type
-  TRestWebService = class(TComponent)
+  TRestWebService = class(TComponent, IGetWebAppServices, IWebAppServices)
+  private
+    // IGetWebAppServices
+    function GetWebAppServices: IWebAppServices;
 
+    // IWebAppServices
+    function GetActive: Boolean;
+    function GetExceptionHandler: TObject;
+    function HandleRequest: Boolean;
+    procedure FinishContext;
+    procedure InitContext(WebModule: TComponent; Request: TWebRequest; Response: TWebResponse);
   end;
 
 implementation
+
+{ TRestWebService }
+
+procedure TRestWebService.FinishContext;
+begin
+
+end;
+
+function TRestWebService.GetActive: Boolean;
+begin
+  Result := False;
+end;
+
+function TRestWebService.GetExceptionHandler: TObject;
+begin
+  Result := nil;
+end;
+
+function TRestWebService.GetWebAppServices: IWebAppServices;
+begin
+  Result := Self;
+end;
+
+function TRestWebService.HandleRequest: Boolean;
+begin
+  Result := False;
+end;
+
+procedure TRestWebService.InitContext(WebModule: TComponent; Request: TWebRequest; Response: TWebResponse);
+begin
+
+end;
 
 end.
