be48b100561c8b6ba024fa72b8a5fcba50d315d2
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2021-09-01 14:00:38 -0300

    Changed outputs.

--- Delphi.Rest.Service.Container.pas
+++ Delphi.Rest.Service.Container.pas
@@ -7,7 +7,7 @@ uses System.Rtti;
 type
   IServiceContainer = interface
     ['{B34DEF5C-A790-4DD6-B47C-506AEF7DDC27}']
-    function GetService(const Name: String; out Info: TRttiType; out Instance: TValue): Boolean;
+    function GetService(const Name: String; var Info: TRttiType; var Instance: TValue): Boolean;
   end;
 
 implementation
