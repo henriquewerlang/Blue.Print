de277765e2877b8ced955632f8bc051192b6779d
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2021-11-23 07:22:20 -0300

    Just a clean up in the test project.

--- Test/Delphi.Rest.Test.dpr
+++ Test/Delphi.Rest.Test.dpr
@@ -1,17 +1,11 @@
 ﻿program Delphi.Rest.Test;
 
-{$IFNDEF TESTINSIGHT}
-{$APPTYPE CONSOLE}
-{$ENDIF}
 {$STRONGLINKTYPES ON}
+
 uses
   FastMM5 in '..\Externals\FastMM5\FastMM5.pas',
   System.SysUtils,
-  {$IFDEF TESTINSIGHT}
   TestInsight.DUnitX,
-  {$ELSE}
-  DUnitX.Loggers.Console,
-  {$ENDIF }
   DUnitX.TestFramework,
   DUnitX.MemoryLeakMonitor.FastMM5,
   Delphi.Rest.Server.Service in '..\Delphi.Rest.Server.Service.pas',
@@ -23,14 +17,6 @@ uses
   Delphi.Rest.Content.Parser.Test in 'Delphi.Rest.Content.Parser.Test.pas',
   Delphi.Rest.Request.Mock in 'Delphi.Rest.Request.Mock.pas';
 
-// Para não remover os itens abaixo.
-{$IFNDEF TESTINSIGHT}
-var
-  runner: ITestRunner;
-  results: IRunResults;
-  logger: ITestLogger;
-  nunitLogger : ITestLogger;
-{$ENDIF}
 begin
   FastMM_OutputDebugStringEvents := [];
   FastMM_LogToFileEvents := [mmetUnexpectedMemoryLeakSummary, mmetUnexpectedMemoryLeakDetail];
@@ -40,46 +26,5 @@ begin
 
   FastMM_EnterDebugMode;
 
-{$IFDEF TESTINSIGHT}
   TestInsight.DUnitX.RunRegisteredTests;
-{$ELSE}
-  try
-    //Check command line options, will exit if invalid
-    TDUnitX.CheckCommandLine;
-    //Create the test runner
-    runner := TDUnitX.CreateRunner;
-    //Tell the runner to use RTTI to find Fixtures
-    runner.UseRTTI := True;
-    //When true, Assertions must be made during tests;
-    runner.FailsOnNoAsserts := False;
-
-    //tell the runner how we will log things
-    //Log to the console window if desired
-    if TDUnitX.Options.ConsoleMode <> TDunitXConsoleMode.Off then
-    begin
-      logger := TDUnitXConsoleLogger.Create(TDUnitX.Options.ConsoleMode = TDunitXConsoleMode.Quiet);
-      runner.AddLogger(logger);
-    end;
-    //Generate an NUnit compatible XML File
-    nunitLogger := TDUnitXXMLNUnitFileLogger.Create(TDUnitX.Options.XMLOutputFile);
-    runner.AddLogger(nunitLogger);
-
-    //Run tests
-    results := runner.Execute;
-    if not results.AllPassed then
-      System.ExitCode := EXIT_ERRORS;
-
-    {$IFNDEF CI}
-    //We don't want this happening when running under CI.
-    if TDUnitX.Options.ExitBehavior = TDUnitXExitBehavior.Pause then
-    begin
-      System.Write('Done.. press <Enter> key to quit.');
-      System.Readln;
-    end;
-    {$ENDIF}
-  except
-    on E: Exception do
-      System.Writeln(E.ClassName, ': ', E.Message);
-  end;
-{$ENDIF}
 end.
