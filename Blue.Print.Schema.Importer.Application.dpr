2bf5ec3ecf1c07fbea723c167b18d96dfa1fff91
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2025-03-27 07:51:08 -0300

    Removed the debug params and removed Delphi unit.

--- Blue.Print.Schema.Importer.Application.dpr
+++ Blue.Print.Schema.Importer.Application.dpr
@@ -5,7 +5,6 @@ uses
   Vcl.Forms,
   Blue.Print.Schema.Importer.Main in 'Blue.Print.Schema.Importer.Main.pas' {Main},
   Blue.Print.Schema.Importer in 'Blue.Print.Schema.Importer.pas',
-  Xml.XMLSchema in 'c:\program files (x86)\embarcadero\studio\23.0\SOURCE\XML\Xml.XMLSchema.pas',
   Blue.Print.Serializer in 'Blue.Print.Serializer.pas',
   Blue.Print.Types in 'Blue.Print.Types.pas';
 
