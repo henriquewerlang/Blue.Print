39c40288495d30ab464994c3eea0b3af839389b0
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2026-05-06 15:14:24 -0300

    Just stared the implementation of Open API 3.0.

--- Blue.Print.Schema.Importer.Open.API.v30.pas
+++ Blue.Print.Schema.Importer.Open.API.v30.pas
@@ -2,11 +2,14 @@
 
 interface
 
-uses Blue.Print.Schema.Importer;
+uses Blue.Print.Schema.Importer, Blue.Print.Open.API.Schema.v30;
 
 type
   TOpenAPI30SchemaLoader = class(TInterfacedObject, ISchemaLoader)
   private
+    FImporter: TSchemaImporter;
+    FOpenAPIDefinition: TOpenAPIDefinition;
+
     procedure GenerateUnitFileDefinition(const UnitDefinition: TUnitDefinition; const UnitFileConfiguration: TUnitFileConfiguration);
   public
     constructor Create(const Importer: TSchemaImporter);
@@ -14,17 +17,30 @@ type
 
 implementation
 
+uses Blue.Print.Serializer, Blue.Print.Types;
+
 { TOpenAPI30SchemaLoader }
 
 constructor TOpenAPI30SchemaLoader.Create(const Importer: TSchemaImporter);
 begin
   inherited Create;
+
+  FImporter := Importer;
 end;
 
 procedure TOpenAPI30SchemaLoader.GenerateUnitFileDefinition(const UnitDefinition: TUnitDefinition; const UnitFileConfiguration: TUnitFileConfiguration);
+
+  procedure LoadDefinition;
+  begin
+    var Serializer := TBluePrintJsonSerializer.Create as IBluePrintSerializer;
+
+    FOpenAPIDefinition := Serializer.Deserialize(FImporter.LoadFileFromConfiguration(UnitFileConfiguration), TypeInfo(TOpenAPIDefinition)).AsType<TOpenAPIDefinition>;
+  end;
+
 begin
   inherited;
 
+  LoadDefinition;
 end;
 
 end.
